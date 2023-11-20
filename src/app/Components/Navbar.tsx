4373fd6312202a7a49056b26341713ebe3580a8b
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2023-11-20 12:20:32 +0100

    all style setup and navbar started

--- src/app/Components/Navbar.tsx
+++ src/app/Components/Navbar.tsx
@@ -1,7 +1,9 @@
 export default function Navbar() {
     return (
         <nav className="main_nav">
-            
+            <div className="logo_container">
+                <img src="/next.svg" alt="Logo" className="logo" />
+            </div>
         </nav>
     )
 }
