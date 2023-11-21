46d722886c1b606737f4a64111545d3e979fd594
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2023-11-21 11:23:52 +0100

    card component done

--- src/app/Components/Card.tsx
+++ src/app/Components/Card.tsx
@@ -1,7 +1,9 @@
-export default function Card() {
+export default function Card({url, name}:{url:string, name:string}) {
   return (
     <div className="card">
-        
+       <div className="head" title={name}>
+          <img src={url} alt="picture" className="card-image" />
+        </div> 
     </div>
   )
 }
