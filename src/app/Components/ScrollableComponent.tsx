c81b7971f9cfb4b81101c64fa063af05b9157178
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2023-11-21 11:30:58 +0100

    added json file for text and cards

--- src/app/Components/ScrollableComponent.tsx
+++ src/app/Components/ScrollableComponent.tsx
@@ -1,13 +1,15 @@
 import { ICard, ICards } from "@/types/card";
 import Card from "./Card";
 
-export default function ScrollableComponent({title, cards}:{title:string, cards:ICards}) {
+export default function ScrollableComponent({title, cards}:{title:string, cards:any}) {
   return (
     <div className="scrollable-component">
         <h3 className="__title">{title}</h3>
         <div className="cards">
             {cards.map((card:ICard) => {
-
+              return (
+                <Card url={card.url} name={card.name} />
+              )
             })}
         </div>
     </div>
--- src/types/card.d.ts
+++ src/types/card.d.ts
@@ -1,4 +1,5 @@
 export interface ICards
+{
     [
         {
             url:string, 
